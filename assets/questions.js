50826f85248d5be803404d36bc8382aa69191d89
mrjakeschultz <111388769+mrjakeschultz@users.noreply.github.com>
2022-10-08 12:49:21 -0500

    created questions questions array

--- assets/questions.js
+++ assets/questions.js
@@ -1,3 +1,93 @@
 //TODO make var array of questions
 //can make keys and properties
 //4 is good number
+
+var questions = [
+	{
+		title: "What is 1?",
+		responses: [
+			{
+				title: "1",
+				isCorrect: true,
+			},
+			{
+				title: "2",
+				isCorrect: false,
+			},
+			{
+				title: "3",
+				isCorrect: false,
+			},
+			{
+				title: "4",
+				isCorrect: false,
+			},
+		],
+	},
+
+	{
+		title: "What is 2?",
+		responses: [
+			{
+				title: "1",
+				isCorrect: false,
+			},
+			{
+				title: "2",
+				isCorrect: true,
+			},
+			{
+				title: "3",
+				isCorrect: false,
+			},
+			{
+				title: "4",
+				isCorrect: false,
+			},
+		],
+	},
+
+	{
+		title: "What is 3?",
+		responses: [
+			{
+				title: "1",
+				isCorrect: false,
+			},
+			{
+				title: "2",
+				isCorrect: false,
+			},
+			{
+				title: "3",
+				isCorrect: true,
+			},
+			{
+				title: "4",
+				isCorrect: false,
+			},
+		],
+	},
+
+	{
+		title: "What is 4?",
+		responses: [
+			{
+				title: "1",
+				isCorrect: false,
+			},
+			{
+				title: "2",
+				isCorrect: false,
+			},
+			{
+				title: "3",
+				isCorrect: false,
+			},
+			{
+				title: "4",
+				isCorrect: true,
+			},
+		],
+	},
+];
